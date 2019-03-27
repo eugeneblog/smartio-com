4a5b71453087e6dcbf3956fe46a775e6b0ed0479
eugeneblog <714284150@qq.com>
2019-03-27 15:55:17 +0800

    update

--- src/pages/product/index.vue
+++ src/pages/product/index.vue
@@ -5,28 +5,12 @@
               <div class="feature-wrapper">
                   <h2>产品中心</h2>
                   <p>Anything embarrassing hide in the middle of text. All the Lorem Ipsum generators on</p>
-                  <el-col :span="14">
-                      <el-col :span="12" class="single-feature">
-                          <img src="../../assets/cases/1.png" alt="">
-                          <h4>温湿度控制</h4>
-                          <p>合理调节温度、湿度、空气质量，在满足舒适度的前提下降低能耗是楼宇自控系统的主要任务之一。</p>
-                      </el-col>
-                      <el-col :span="12" class="single-feature">
-                          <img src="../../assets/cases/2.png" alt="">
-                          <h4>照明控制</h4>
-                          <p>附属地下停车场照明、通风、污水管理，商场内照明管理和商场景观照明管理也是减少能耗、降低设备管理人员劳动力成本的重要因素。</p>
-                      </el-col>
-                      <el-col :span="12" class="single-feature">
-                          <img src="../../assets/cases/3.png" alt="">
-                          <h4>节能环保</h4>
-                          <p>我们需要建设可持续的能源系统，尽可能的做到最大的节能</p>
-                      </el-col>
-                      <el-col :span="12" class="single-feature">
-                          <img src="../../assets/cases/4.png" alt="">
-                          <h4>节省人力</h4>
-                          <p>百分之八十以上的设备都实现了自动化管理</p>
-                      </el-col>
-                  </el-col>
+                  <div class="single-feature" v-for="(o, index) of childrens" :key="index">
+                      <img src="../../assets/cases/1.png" alt="">
+                      <h4>{{o.text}}</h4>
+                      <p></p>
+                      <router-link :to="o.link">{{o.text}}>>></router-link>
+                  </div>
               </div>
           </el-container>
       </div>
@@ -62,7 +46,6 @@ export default {
     -webkit-background-size: cover;
     background-size: cover;
     width: 100%;
-    z-index: -1;
     position: relative;
     &::before {
         background-color: #00b0b0;
@@ -83,6 +66,9 @@ export default {
             padding-right: 60px;
             h4 {
                 color: #ffffff;
+            }
+            a {
+              color: #ffffff;
             }
         }
         h2 {
